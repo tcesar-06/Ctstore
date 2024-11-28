92aedfca74574d46ed77b35196d03f679095a6c5
tcesar-06 <tcesardev@gmail.com>
2024-11-28 09:59:39 -0300

    Thiago: terminei o estoque jogador

---
+++ src/app/pages/Estoque/estoque-jogador/estoque-jogador.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-estoque-jogador',
+  standalone: true,
+  imports: [],
+  templateUrl: './estoque-jogador.component.html',
+  styleUrl: './estoque-jogador.component.css'
+})
+export class EstoqueJogadorComponent {
+
+}
