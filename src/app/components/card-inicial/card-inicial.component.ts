63febf2cb1a78e8917e747470b9518698415eb3e
tcesar-06 <tcesardev@gmail.com>
2024-11-27 22:01:10 -0300

    Thiago: Fiz o estoque com for e fiz a alteração do home, coloqueim um for tambem

--- src/app/components/card-inicial/card-inicial.component.ts
+++ src/app/components/card-inicial/card-inicial.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-inicial',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card-inicial.component.html',
   styleUrl: './card-inicial.component.css'
 })
 export class CardInicialComponent {
 
+  @Input() imagem: string = '';
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,14 +5,32 @@ import { LayoutHeaderComponent } from '../../layout/layout-header/layout-header.
 import { BodyComponent } from '../../components/body/body.component';
 import { RouterOutlet } from '@angular/router';
 import { CardInicialComponent } from '../../components/card-inicial/card-inicial.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [LayoutBotoesComponent, LayoutFooterComponent, LayoutHeaderComponent, BodyComponent, RouterOutlet, CardInicialComponent],
+  imports: [LayoutBotoesComponent, LayoutFooterComponent, LayoutHeaderComponent, BodyComponent, RouterOutlet, CardInicialComponent, CommonModule],
 templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  produtos = [
+    {
+      imagem: '/assets/image/torcedor-argentina-1-2024.jpg'
+    },
+    {
+      imagem: '/assets/image/torcedor-flamengo-conciencia_negra-2024.jpg'
+    },
+    {
+      imagem: '/assets/image/torcedor-corinthians-1-2024.jpg'
+    },
+    {
+      imagem: '/assets/image/torcedor-inter_milan-1-2024.jpeg'
+    },
+    {
+      imagem: '/assets/image/torcedor-vasco-100anos-2024.jpg'
+    }
+  ]
 }
 
